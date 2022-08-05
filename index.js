e4cb0ab8ce6c7c294bb573ff7901137f5bb635dd
mohak chutani <mohakchutani1@gmail.com>
2022-08-05 16:56:42 +0530

    chnages

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ require("./database/config");
 const User = require("./database/user")
 
 app = express();
-app.use(cors());
-
+app.use(cors({
+  origin: ["https://master--bespoke-cranachan-6caef0.netlify.app/"], // the link of my front-end app on Netlify
+  methods: ["GET", "POST"],
+  credentials: true
+}))
 
 app.use(express.json())
 app.post("/",async(req,resp)=>{
