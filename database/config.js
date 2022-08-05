30a6c9948655e0ae1803c03833f1e11e134b065e
mohak chutani <mohakchutani1@gmail.com>
2022-08-05 16:31:03 +0530

    changes in config

--- database/config.js
+++ database/config.js
@@ -1,2 +1,3 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://0.0.0.0:27017/global-legal-association');
+mongoose.connect('mongodb+srv://mohak00:mohak_0000@cluster0.tlalbb1.mongodb.net/?retryWrites=true&w=majority');
+//mongoose.connect('mongodb://0.0.0.0:27017/global-legal-association');
