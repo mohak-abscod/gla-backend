e548260f138a75705d0f3984f2a728fc31bc8028
mohak chutani <mohakchutani1@gmail.com>
2022-08-05 15:58:02 +0530

    first commit

---
+++ database/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const formSchema = new mongoose.Schema({
+firstName: String,
+    lastName:String,
+    email: String,
+    companyName: String,
+    phone: String,
+    passes: String,
+    exhibition:String,
+    sponsorAssets:String
+});
+module.exports = mongoose.model("forms",formSchema);
