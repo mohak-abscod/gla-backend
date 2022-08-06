1ecf225a498468dde226e616624d1602cf492293
mohak chutani <mohakchutani1@gmail.com>
2022-08-06 16:51:01 +0530

    ff

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
 require("./database/config");
-const User = require("./database/user")
+const User = require("./database/user");
 
+//app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://62ee4cec9772e9000905a3b4--bespoke-cranachan-6caef0.netlify.app/"
+  ); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.use(
-  cors({
-    origin: ["https://master--bespoke-cranachan-6caef0.netlify.app/"],
-    methods: ["GET", "POST", "DELETE"],
-    credentials: true,
-    origin: true,
-  })
-);
-app.use(express.json())
-app.post("/",async(req,resp)=>{
-   let get = new User(req.body);
-   let result = await get.save();
-   resp.send(result);
-})
+app.use(express.json());
+app.post("/", async (req, resp) => {
+  let get = new User(req.body);
+  let result = await get.save();
+  resp.send(result);
+});
 app.listen(5500);
