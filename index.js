aff4ffd55637326da2bbb3647d8bd8ae5036143f
mohak chutani <mohakchutani1@gmail.com>
2022-08-08 16:13:00 +0530

    origin

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ require("./database/config");
 const User = require("./database/user");
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://62ee4cec9772e9000905a3b4--bespoke-cranachan-6caef0.netlify.app/"
-  ); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://62ee4cec9772e9000905a3b4--bespoke-cranachan-6caef0.netlify.app/"
+//   ); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(express.json());
-app.post("/temp", async (req, resp) => {
+app.post("/", async (req, resp) => {
   resp.setHeader("Access-Control-Allow-Methods", "*");
   resp.setHeader("Access-Control-Allow-Headers", "*");
   let get = new User(req.body);
   let result = await get.save();
   resp.send(result);
 });
-app.get("/", async (req, resp) => {
-  resp.send("<h1>Hello world</h1>");
-});
+// app.get("/", async (req, resp) => {
+//   resp.send("<h1>Hello world</h1>");
+// });
 app.listen(5500);
