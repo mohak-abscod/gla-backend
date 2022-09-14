e276f2695fe1cee2744181ffe9f6902226694688
mohak chutani <mohakchutani1@gmail.com>
2022-09-14 20:50:41 +0530

    deployment error saved

--- index.js
+++ index.js
@@ -9,17 +9,7 @@ const app = express();
 
 app.use(cors());
 
-// app.use(function (req, res, next) {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://62ee4cec9772e9000905a3b4--bespoke-cranachan-6caef0.netlify.app/"
-//   ); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
